5b0557e9463c5b3a706158ceca9b2af0b49815a9
rahmantamim11 <tauraxtamim11@gmail.com>
2024-03-07 16:50:00 +0600

    next js project done

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Music Lounge",
+  description: "This is a Music Academy Website",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body className={inter.className}>
+        <div className="relative w-full flex items-center justify-center ">
+          <Navbar />
+        </div>
+        {children}
+      </body>
+    </html>
+  );
+}
